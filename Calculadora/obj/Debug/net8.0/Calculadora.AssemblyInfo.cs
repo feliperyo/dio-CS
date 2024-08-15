20d62c227725880da1d3ed86c2ecd3d4f5195d96
feliperyo <ryofeliperyo@gmail.com>
2024-08-15 15:39:51 -0300

    debug

--- Calculadora/obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
+++ Calculadora/obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc4cc26088fbb594c71705109f4306da3f9c1695")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e42cbe041c6dc1f389e8c515057624472321bee2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
