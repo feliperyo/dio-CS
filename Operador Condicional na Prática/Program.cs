578703d00d79f5a1a9f585e0f353a863da7affff
feliperyo <ryofeliperyo@gmail.com>
2024-08-15 09:30:15 -0300

    if e else

--- Operador Condicional na Prática/Program.cs
+++ Operador Condicional na Prática/Program.cs
@@ -1,9 +1,13 @@
 ﻿
 int quantidadeEmEstoque = 10;
-
 int quantidadeCompra = 54;
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
 
-if (quantidadeEmEstoque >= quantidadeCompra)
+Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+
+if (possivelVenda)
 {
     Console.WriteLine("Venda realizada.");
 }
