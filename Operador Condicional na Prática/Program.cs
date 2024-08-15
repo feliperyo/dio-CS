f76808b925a5825aa94e504f6d424c51daed4fc3
feliperyo <ryofeliperyo@gmail.com>
2024-08-15 09:23:59 -0300

    if e else

---
+++ Operador Condicional na Prática/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int quantidadeEmEstoque = 10;
+
+int quantidadeCompra = 54;
+
+if (quantidadeEmEstoque >= quantidadeCompra)
+{
+    Console.WriteLine("Venda realizada.");
+}
+else
+{
+    Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque.");
+}
