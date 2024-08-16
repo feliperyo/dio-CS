4bd51fe845c9413062576f1e11cd721fa8141d8f
feliperyo <ryofeliperyo@gmail.com>
2024-08-16 11:32:53 -0300

    WHILE

--- Estruturas de repetição/Program.cs
+++ Estruturas de repetição/Program.cs
@@ -1,9 +1,19 @@
 ﻿// FOR
 
-int numero = 5;
+// int numero = 5;
 
-for (int contador = 0; contador <= 10; contador++)
+// for (int contador = 0; contador <= 10; contador++)
+// {
+//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+// }
+
+// WHILE
+
+int numero = 5;
+int contador = 0;
+
+while (contador <= 10)
 {
     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+    contador++;
 }
-
