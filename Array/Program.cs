251c5b605ae4e5e750c9afa55aa72cdd0fa0390e
feliperyo <ryofeliperyo@gmail.com>
2024-08-19 08:34:43 -0300

    redimensionando o Array

--- Array/Program.cs
+++ Array/Program.cs
@@ -6,6 +6,10 @@ arrayInteiros[1] = 64;
 arrayInteiros[2] = 50;
 arrayInteiros[3] = 1;
 
+
+//Redimensionando o Array
+Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+
 Console.WriteLine("Percorrendo o Array com FOR");
 for (int contador = 0; contador < arrayInteiros.Length; contador++)
 {
