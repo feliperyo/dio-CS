c7671388baccc5fc20093b98b00355da1f7eff15
feliperyo <ryofeliperyo@gmail.com>
2024-08-19 09:23:58 -0300

    Listas

---
+++ Lista/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.Serialization.Formatters;
+
+List<string> listaString = new List<string>();
+
+listaString.Add("SP");
+listaString.Add("BA");
+listaString.Add("MG");
+
+
+Console.WriteLine("Percorrendo o Array com FOR");
+
+for (int contador = 0; contador < listaString.Count; contador++)
+{
+    Console.WriteLine($"Posição Nº{contador} - {listaString[contador]}");
+}
+
+Console.WriteLine("Percorrendo o Array com FOREACH");
+
+int contadorForeach = 0;
+foreach (string item in listaString)
+{
+    Console.WriteLine($"Posição Nº{contadorForeach} - {item}");
+    contadorForeach++;
+}
--- Tipo Date Time/obj/Debug/net8.0/Tipo Date Time.AssemblyInfo.cs
+++ Tipo Date Time/obj/Debug/net8.0/Tipo Date Time.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tipo Date Time")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99bcd97b2011fd2022823ce7c13a4d0ced099ace")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e93dd542d73891f100661facc53fa044bf04a21")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tipo Date Time")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tipo Date Time")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
