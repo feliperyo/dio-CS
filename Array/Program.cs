99bcd97b2011fd2022823ce7c13a4d0ced099ace
feliperyo <ryofeliperyo@gmail.com>
2024-08-19 08:33:16 -0300

    Array

---
+++ Array/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int[] arrayInteiros = new int[4];
+
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 64;
+arrayInteiros[2] = 50;
+arrayInteiros[3] = 1;
+
+
+//Redimencionando o Array
+Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+
+Console.WriteLine("Percorrendo o Array com FOR");
+for (int contador = 0; contador < arrayInteiros.Length; contador++)
+{
+    Console.WriteLine($"Posição Nº{contador} - {arrayInteiros[contador]}");
+}
+
+Console.WriteLine("Percorrendo o Array com FOREACH");
+foreach (int valor in arrayInteiros)
+{
+    Console.WriteLine(valor);
+}
