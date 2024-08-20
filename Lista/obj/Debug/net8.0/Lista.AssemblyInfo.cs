04edd97918e262fa27c4721f0ebf951db710af18
feliperyo <ryofeliperyo@gmail.com>
2024-08-20 10:42:49 -0300

    debug

--- Lista/obj/Debug/net8.0/Lista.AssemblyInfo.cs
+++ Lista/obj/Debug/net8.0/Lista.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lista")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e99e004a0ca2d0f81fb5e20a2fd3b2b6f4a2b3b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3028c87bf99dfc9bbd8d6c5df5a9681f3ad938")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lista")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lista")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Sintaxe .NET C#/obj/Debug/net8.0/Sintaxe .NET C#.AssemblyInfo.cs
+++ Sintaxe .NET C#/obj/Debug/net8.0/Sintaxe .NET C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sintaxe .NET C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e99e004a0ca2d0f81fb5e20a2fd3b2b6f4a2b3b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3028c87bf99dfc9bbd8d6c5df5a9681f3ad938")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sintaxe .NET C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sintaxe .NET C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
